4c041987e1be17a8ab15b368712825778d62968a
Arata Tanaka <Arata_Tanaka@voyagegroup.com>
2015-04-17 20:50:13 +0900

    改行コードを置換するようにした

--- query/get_messages.sh
+++ query/get_messages.sh
@@ -4,7 +4,7 @@ while :
 do
     s=`dirname $0`
     last=`cat $s/last_timestamp`
-    q="select author, replace(body_xml, '\\n', '\\\\n') from Messages where chatname = '#a.macbee/\$kay_t_arata;139ebf66853db51f' and timestamp > $last;"
+    q="select author, replace(body_xml, '\\n', ' ') from Messages where chatname = '#a.macbee/\$kay_t_arata;139ebf66853db51f' and timestamp > $last;"
     echo $q|sqlite3 ~/Library/Application\ Support/Skype/kay_t_arata/main.db >> $s/../messages
     sh $s/get_last_timestamp.sh
     sleep 2
