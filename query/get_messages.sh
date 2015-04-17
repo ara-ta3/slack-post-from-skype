2ace1c67199442a35ab4d3c5978dba9856267cdd
Arata Tanaka <Arata_Tanaka@voyagegroup.com>
2015-04-17 21:34:08 +0900

    bot用アカウントに変更

--- query/get_messages.sh
+++ query/get_messages.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
+target='aratabot'
 while :
 do
     s=`dirname $0`
     last=`cat $s/last_timestamp`
-    q="select author, replace(body_xml, '\\n', ' ') from Messages where chatname = '#a.macbee/\$kay_t_arata;139ebf66853db51f' and timestamp > $last;"
-    echo $q|sqlite3 ~/Library/Application\ Support/Skype/kay_t_arata/main.db >> $s/../messages
+    q="select author, replace(body_xml, '\\n', ' ') from Messages where chatname = '#a.macbee/\$kay_t_arata;139ebf66853db51f' and not author = '$target' and timestamp > $last;"
+    echo $q|sqlite3 ~/Library/Application\ Support/Skype/$target/main.db >> $s/../messages
     sh $s/get_last_timestamp.sh
     sleep 2
 done
---
+++ skypescript/post.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+name=$1
+text=$2
+echo "
+tell application \"Skype\"
+    send command \"CHATMESSAGE #a.macbee/\$kay_t_arata;139ebf66853db51f $name)< $text\" script name \"skype script\"
+end tell
+" | osascript -
